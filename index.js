03b913822a839c76abaa0aa79dfdc4f55595228f
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-05 13:28:15 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName  = "bob"
+
+function upperCaseCustomerName(){
+   return customerName = "BOB";
+}
+
+
+function setBestCustomer(){
+    return bestCustomer = "not bob"
+}
+
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = 'Ali';
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'Veli'
+}
